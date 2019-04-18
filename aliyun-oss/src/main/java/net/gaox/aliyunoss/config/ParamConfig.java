abc507064b2a452bbe1c9ac34a29b9f82af2b2a8
gaoxizhi <gx_zone@163.com>
2019-04-18 23:31:59 +0800

    添加阿里云OSS模块

---
+++ aliyun-oss/src/main/java/net/gaox/aliyunoss/config/ParamConfig.java
@@ -0,0 +1,18 @@
+package net.gaox.aliyunoss.config;
+
+/**
+ * @Description: <p>  </p>
+ * @ClassName ParamConfig
+ * @Author: gaox·Eric
+ * @Date: 2019/4/18 23:02
+ */
+public class ParamConfig {
+    /**
+     * 注意：这里记录的是自己的Oss账号信息
+     */
+    public static String endpoint;
+    public static String accessKeyId;
+    public static String accessKeySecret;
+    public static String bucketName;
+    public static String folder;
+}
