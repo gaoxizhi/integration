bc60b88b34d7eaf037ddd1fb1921ecb7b40fc627
高羲之 <www.gx_zone@126.com>
2019-07-14 23:12:43 +0800

    拦截器分页（未完成）

---
+++ page/src/main/java/net/gaox/page/controller/UserController.java
@@ -0,0 +1,43 @@
+package net.gaox.page.controller;
+
+
+import net.gaox.page.entity.User;
+import net.gaox.page.mapper.UserMapper;
+import net.gaox.page.service.UserService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ * 基础用户表 前端控制器
+ * </p>
+ *
+ * @author: gaox·Eric
+ * @since : 2019/7/9
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/list")
+    public List<User> list(@RequestParam Integer page, @RequestParam Integer limit) {
+        List<User> list = userService.listGaoPage(page, limit);
+//        list.stream().forEach(s -> System.out.println(s.toString()));
+        return list;
+    }
+
+}
+
---
+++ page/src/main/java/net/gaox/page/entity/User.java
@@ -0,0 +1,72 @@
+package net.gaox.page.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 基础用户表
+ * </p>
+ *
+ * @author GengYun
+ * @since 2019-07-09
+ */
+@Data
+public class User {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String name;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+
+    /**
+     * 出生日期
+     */
+    private LocalDate birthDate;
+
+    /**
+     * 性别：女0；男1
+     */
+    private Boolean gender;
+
+    /**
+     * 工资
+     */
+    private BigDecimal salary;
+
+    /**
+     * 地址
+     */
+    private String address;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 上次修改时间
+     */
+    private LocalDateTime updateTime;
+
+    /**
+     * 删除标志：删除0；正常1（默认）
+     */
+    private Boolean delFlag;
+
+}
