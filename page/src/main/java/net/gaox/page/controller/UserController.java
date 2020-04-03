1183387f25f5f2c6377420f25f8a40770a7cfc85
gaox <gx_zone@126.com>
2020-04-04 01:49:35 +0800

    [dev] 部分信息调整

--- page/src/main/java/net/gaox/page/controller/UserController.java
+++ page/src/main/java/net/gaox/page/controller/UserController.java
@@ -2,15 +2,12 @@ package net.gaox.page.controller;
 
 
 import net.gaox.page.entity.User;
-import net.gaox.page.mapper.UserMapper;
 import net.gaox.page.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -25,16 +22,16 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    @Resource
-    private UserMapper userMapper;
+    private final UserService userService;
 
-    @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/list")
     public List<User> list(@RequestParam Integer page, @RequestParam Integer limit) {
         List<User> list = userService.listGaoPage(page, limit);
-//        list.stream().forEach(s -> System.out.println(s.toString()));
+        list.stream().forEach(s -> System.out.println(s.toString()));
         return list;
     }
 }
