9f7a83cafe643ebf8d5aab86eac521abc82100cf
gaox <gx_zone@126.com>
2020-05-23 01:40:11 +0800

    [dev] 聊天室调整

--- live-chat-netty/src/main/java/net/gaox/livechat/handler/NettyWebSocketChannelInitializer.java
+++ live-chat-netty/src/main/java/net/gaox/livechat/handler/NettyWebSocketChannelInitializer.java
@@ -10,7 +10,6 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -22,25 +21,46 @@ import org.springframework.stereotype.Component;
 @Component
 @Qualifier("somethingChannelInitializer")
 public class NettyWebSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
-//    private RabbitSender rabbitSender;
+    //    private RabbitSender rabbitSender;
 //    private int port;
 //
 //    public NettyWebSocketChannelInitializer(RabbitSender rs, int tcpPort) {
 //        this.rabbitSender = rs;
 //        this.port = tcpPort;
 //    }
-    @Autowired
-    private TextWebSocketFrameHandler textWebSocketFrameHandler;
+    private final TextWebSocketFrameHandler textWebSocketFrameHandler;
+
+    public NettyWebSocketChannelInitializer(TextWebSocketFrameHandler textWebSocketFrameHandler) {
+        this.textWebSocketFrameHandler = textWebSocketFrameHandler;
+    }
 
 
     @Override
     public void initChannel(SocketChannel ch) throws Exception {//2
         ChannelPipeline pipeline = ch.pipeline();
 
+        // ====================== 用于支持http协议    ======================
+
+        // websocket 基于http协议，所以要有http编解码器
         pipeline.addLast(new HttpServerCodec());
+        // 对httpMessage进行聚合，聚合成FullHttpRequest或FullHttpResponse
+        // 几乎在netty中的编程，都会使用到此hanler
         pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+        // 对写大数据流的支持
         pipeline.addLast(new ChunkedWriteHandler());
+
+
+        // ====================== 支持httpWebsocket ======================
+
+        /**
+         * websocket 服务器处理的协议，用于指定给客户端连接访问的路由 : /ws
+         * 本handler会帮你处理一些繁重的复杂的事
+         * 会帮你处理握手动作： handshaking（close, ping, pong） ping + pong = 心跳
+         * 对于websocket来讲，都是以frames进行传输的，不同的数据类型对应的frames也不同
+         */
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+
+        // 自定义handler 文本处理器
         pipeline.addLast(textWebSocketFrameHandler);
         //分割符
         ByteBuf delimiter = Unpooled.copiedBuffer(new byte[]{-25});
--- live-chat-netty/src/main/java/net/gaox/livechat/handler/TextWebSocketFrameHandler.java
+++ live-chat-netty/src/main/java/net/gaox/livechat/handler/TextWebSocketFrameHandler.java
@@ -8,23 +8,22 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import net.gaox.livechat.entity.User;
-import net.gaox.livechat.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import net.gaox.livechat.util.RandomName;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
+ * <p> 处理[文本消息]的handler </p>
+ * {@link TextWebSocketFrame} 在netty中，是用于为websocket专门处理文本的对象，frame是消息的载体
+ *
  * @author gaox·Eric
  * @date 2019/7/19 20:25
  */
+@Slf4j
 @Component
 @Qualifier("textWebSocketFrameHandler")
 @ChannelHandler.Sharable
@@ -34,66 +33,79 @@ public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextW
 
     public static Map<String, String> map = new HashMap<>(128);
 
-    @Autowired
-    private UserService userService;
-
     @Override
     protected void channelRead0(ChannelHandlerContext ctx,
                                 TextWebSocketFrame msg) throws Exception {
         Channel incoming = ctx.channel();
-        String uName = map.get(incoming.id().toString());
+        String uName = map.get(incoming.id().asLongText());
         for (Channel channel : channels) {
             if (channel != incoming) {
                 channel.writeAndFlush(new TextWebSocketFrame("[" + uName + "]" + msg.text()));
             } else {
-                System.out.println("66666666");
                 channel.writeAndFlush(new TextWebSocketFrame("[you]" + msg.text()));
-                System.out.println(String.format("%s  User[%s]: %s", LocalDateTime.now().toString(), uName, msg.text()));
+                log.info("收到来自[{}]的消息：[{}]", uName, msg.text());
             }
         }
     }
+
+    /**
+     * 当客户端连接服务端之后（打开连接）
+     * 获取客户端的channle，并且放到ChannelGroup中去进行管理
+     *
+     * @param ctx ctx
+     * @throws Exception e
+     */
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        System.out.println(ctx.channel().remoteAddress());
+
         String uName = new RandomName().getRandomName();
-        BigDecimal decimal = new BigDecimal("0");
         Channel incoming = ctx.channel();
-        System.out.println("333333333333333333333");
-        List<User> twoName = userService.findTwoName("小红", "笑话");
-        twoName.forEach(s -> System.out.println(s.toString() + "1111111111111111111111111111111111111"));
+        map.put(incoming.id().asLongText(), uName);
+        log.info("新用户[{}]加入，来自[{}]", map.get(incoming.id().asLongText()), ctx.channel().remoteAddress().toString());
+
         for (Channel channel : channels) {
             channel.writeAndFlush(new TextWebSocketFrame("[新用户] - " + uName + " 加入"));
         }
-        map.put(incoming.id() + "", uName);
         channels.add(ctx.channel());
     }
+
+    /**
+     * 当触发handlerRemoved，ChannelGroup会自动移除对应客户端的channel
+     *
+     * @param ctx ctx
+     * @throws Exception e
+     */
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
         Channel incoming = ctx.channel();
-        String uName = map.get(incoming.id().toString());
+        String uName = map.get(incoming.id().asLongText());
+        log.info("用户[{}]离开", uName);
+
         for (Channel channel : channels) {
             channel.writeAndFlush(new TextWebSocketFrame("[用户] - " + uName + " 离开"));
         }
-        map.remove(incoming.id().toString());
+        map.remove(incoming.id().asLongText());
 //        redisDao.saveString("cacheName",redisDao.getString("cacheName").replaceAll(uName,""));
 
         channels.remove(ctx.channel());
     }
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         Channel incoming = ctx.channel();
-        System.out.println("用户:" + map.get(incoming.id().toString()) + "在线");
+        log.info("用户[{}]上线", map.get(incoming.id().asLongText()));
     }
+
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         Channel incoming = ctx.channel();
-        System.out.println("用户:" + map.get(incoming.id().toString()) + "掉线");
+        log.info("用户[{}]离线", map.get(incoming.id().asLongText()));
     }
+
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-            throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         Channel incoming = ctx.channel();
-        System.out.println("用户:" + map.get(incoming.id().toString()) + "异常");
+        log.info("用户[{}]状态异常", map.get(incoming.id().asLongText()));
         cause.printStackTrace();
         ctx.close();
     }
