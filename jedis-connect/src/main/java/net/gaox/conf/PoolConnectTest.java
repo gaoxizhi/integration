14d2e4b04d3fa016a2eb07210463393742bfe30e
gaox <gx_zone@163.com>
2024-01-08 11:56:10 +0800

    [dev] JedisPool 配置

--- jedis-connect/src/main/java/net/gaox/conf/PoolConnectTest.java
+++ jedis-connect/src/main/java/net/gaox/conf/PoolConnectTest.java
@@ -1,9 +1,11 @@
 package net.gaox.conf;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * <p> jedis 连接池测试 </p>
@@ -16,13 +18,30 @@ public class PoolConnectTest {
     private static final Logger log = LoggerFactory.getLogger(PoolConnectTest.class);
 
     public static void main(String[] args) {
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+
+        // 设置最大连接数为默认值的5倍
+        poolConfig.setMaxTotal(GenericObjectPoolConfig.DEFAULT_MAX_TOTAL * 5);
+        // 设置最大空闲连接数为默认值的3倍
+        poolConfig.setMaxIdle(GenericObjectPoolConfig.DEFAULT_MAX_IDLE * 3);
+        // 设置最小空闲连接数为默认值的2倍
+        poolConfig.setMinIdle(GenericObjectPoolConfig.DEFAULT_MIN_IDLE * 2);
+        // 设置开启jmx功能
+        poolConfig.setJmxEnabled(true);
+        // 设置连接池没有连接后客户端的最大等待时间(单位为毫秒)
+        poolConfig.setMaxWaitMillis(3000);
+
         // scheme   = redis://
-        // username = clint
+        // username = client
         // password = pass
         // host     = gaox.net
         // port     = 6379
         // database = 0
-        JedisPool jedisPool = new JedisPool("redis://clint:pass@gaox.net:6379/0");
+        // JedisPool jedisPool = new JedisPool(poolConfig, "redis://client:pass@gaox.net:6379/0");
+        JedisPool jedisPool = new JedisPool(poolConfig, "gaox.net", 6379, 3000,
+                "pass", 0, "client");
+
         Jedis jedis = null;
         try {
             // 1. 从连接池获取jedis对象
