e674c45cecfdea249d32243279032b2b6ae0e033
gaox <gx_zone@163.com>
2024-01-18 17:36:45 +0800

    [dev] yield 让出CPU给其他线程执行

--- explore/src/main/java/net/gaox/thread/base/method/ThreadYield.java
+++ explore/src/main/java/net/gaox/thread/base/method/ThreadYield.java
@@ -1,25 +1,31 @@
 package net.gaox.thread.base.method;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.stream.IntStream;
 
 /**
- * <p>  </p>
+ * <p> 让出CPU给其他线程执行: 你人还怪好嘞 </p>
+ * yield()方法并不会释放锁，线程依然处于RUNNABLE状态，线程不会进入堵塞状态
+ * 不能保证在调用后一定能达到预期的效果
  *
  * @author gaox·Eric
  * @date 2021/4/27 22:28
  */
+@Slf4j
 public class ThreadYield {
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, 2).mapToObj(ThreadYield::create).forEach(Thread::start);
+        IntStream.rangeClosed(1, 5).mapToObj(ThreadYield::create).forEach(Thread::start);
     }
 
     private static Thread create(int index) {
         return new Thread(() -> {
+            log.info("index = {}", index);
             // 调用yield方法
-            if (1 == index) {
+            if (1 == index % 3) {
                 Thread.yield();
             }
-            System.out.println(index);
         });
     }
+
 }
