2f84f5b8134fd0ad3425200cb35154d18db7e396
gaox <gx_zone@163.com>
2024-01-28 15:22:30 +0800

    [dev] 线程池，改用 单线程 ScheduledExecutorService 用于监控线程池状态

---
+++ explore/src/main/java/net/gaox/thread/gaoxpool/GaoxPool2.java
@@ -0,0 +1,193 @@
+package net.gaox.thread.gaoxpool;
+
+import lombok.extern.slf4j.Slf4j;
+import net.gaox.thread.gaoxpool.factory.DefaultThreadFactory;
+import net.gaox.thread.gaoxpool.factory.ThreadFactory;
+import net.gaox.thread.gaoxpool.policy.DenyPolicy;
+import net.gaox.thread.gaoxpool.policy.DiscardDenyPolicy;
+import net.gaox.thread.gaoxpool.queue.LinkedRunnableQueue;
+import net.gaox.thread.gaoxpool.queue.RunnableQueue;
+import net.gaox.thread.gaoxpool.task.InternalTask;
+import net.gaox.thread.gaoxpool.task.ThreadTask;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class GaoxPool2 implements ThreadPool {
+
+    private final int initSize;
+
+    private final int coreSize;
+
+    private final int maxSize;
+
+    private int activeCount;
+
+    private final ThreadFactory threadFactory;
+
+    private final RunnableQueue runnableQueue;
+
+    private volatile boolean isShutdown = false;
+
+    private final Queue<ThreadTask> threadQueue = new ArrayDeque<>();
+
+    private final static DenyPolicy DEFAULT_DENY_POLICY = new DiscardDenyPolicy();
+
+    private final static ThreadFactory DEFAULT_THREAD_FACTORY = new DefaultThreadFactory();
+
+    private final long keepAliveTime;
+
+    private final TimeUnit timeUnit;
+    private final ScheduledExecutorService monitor;
+    private final Object monitorLock = new Object();
+
+    public GaoxPool2(int initSize, int coreSize, int maxSize, int queueSize) {
+        this(initSize, coreSize, maxSize,
+                DEFAULT_THREAD_FACTORY, queueSize, DEFAULT_DENY_POLICY, 1, TimeUnit.SECONDS);
+    }
+
+    public GaoxPool2(int initSize, int coreSize, int maxSize,
+                     ThreadFactory threadFactory, int queueSize,
+                     DenyPolicy denyPolicy, long keepAliveTime, TimeUnit timeUnit) {
+        this.initSize = initSize;
+        this.coreSize = coreSize;
+        this.maxSize = maxSize;
+        this.threadFactory = threadFactory;
+        this.runnableQueue = new LinkedRunnableQueue(queueSize, denyPolicy, this);
+        this.keepAliveTime = keepAliveTime;
+        this.timeUnit = timeUnit;
+
+        // 创建一个单线程 ScheduledExecutorService 用于监控线程池状态
+        this.monitor = Executors.newScheduledThreadPool(1);
+        this.monitor.scheduleWithFixedDelay(this::monitorThreadPool, 0, keepAliveTime, timeUnit);
+
+        log.info("thread pool create, param: initSize = {}, coreSize = {}, maxSize = {}, queueSize = {}, " +
+                        "denyPolicy = {}, keepAliveTime = {} {}.",
+                initSize, coreSize, maxSize, queueSize, denyPolicy, keepAliveTime, timeUnit);
+
+        for (int i = 0; i < initSize; i++) {
+            log.info("create to init.");
+            newThread();
+        }
+    }
+
+    private void monitorThreadPool() {
+        synchronized (monitorLock) {
+            if (isShutdown) {
+                return;
+            }
+
+            log.debug("runnableQueue.size = {}, activeCount = {}.", runnableQueue.size(), getActiveCount());
+
+            // 线程池关闭时，无任何操作
+            if (isShutdown) {
+                return;
+            }
+
+            // 任务队列非空时，根据coreSize和maxSize调整线程数
+            while (runnableQueue.size() > 0 && getActiveCount() < coreSize) {
+                log.info("create to coreSize.");
+                newThread();
+            }
+            while (runnableQueue.size() > 0 && getActiveCount() < maxSize) {
+                log.info("create to maxSize.");
+                newThread();
+            }
+
+            // 任务队列空时，根据coreSize减少线程数
+            while (runnableQueue.size() == 0 && getActiveCount() > coreSize) {
+                log.info("runnableQueue is empty remove overmuch active thread.");
+                removeThread();
+            }
+        }
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+        if (this.isShutdown) {
+            throw new IllegalStateException("The thread pool is destroy");
+        }
+        this.runnableQueue.offer(runnable);
+    }
+
+    private void newThread() {
+        InternalTask internalTask = new InternalTask(runnableQueue);
+        Thread thread = this.threadFactory.createThread(internalTask);
+        ThreadTask threadTask = new ThreadTask(thread, internalTask);
+        threadQueue.offer(threadTask);
+        this.activeCount++;
+        log.trace("create a thread.");
+        thread.start();
+    }
+
+    private void removeThread() {
+        ThreadTask threadTask = threadQueue.remove();
+        threadTask.getInternalTask().stop();
+        log.trace("remove a thread.");
+        this.activeCount--;
+    }
+
+    @Override
+    public void shutdown() {
+        synchronized (monitorLock) {
+            if (isShutdown) {
+                return;
+            }
+            isShutdown = true;
+            monitor.shutdownNow();
+            threadQueue.forEach(threadTask -> {
+                threadTask.getInternalTask().stop();
+                threadTask.getThread().interrupt();
+            });
+        }
+    }
+
+    @Override
+    public int getInitSize() {
+        if (isShutdown) {
+            throw new IllegalStateException("The thread pool is destroy");
+        }
+        return this.initSize;
+    }
+
+    @Override
+    public int getMaxSize() {
+        if (isShutdown) {
+            throw new IllegalStateException("The thread pool is destroy");
+        }
+        return this.maxSize;
+    }
+
+    @Override
+    public int getCoreSize() {
+        if (isShutdown) {
+            throw new IllegalStateException("The thread pool is destroy");
+        }
+        return this.coreSize;
+    }
+
+    @Override
+    public int getQueueSize() {
+        if (isShutdown) {
+            throw new IllegalStateException("The thread pool is destroy");
+        }
+        return runnableQueue.size();
+    }
+
+    @Override
+    public int getActiveCount() {
+        synchronized (this) {
+            return this.activeCount;
+        }
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return this.isShutdown;
+    }
+
+}
